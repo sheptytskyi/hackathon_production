21e8e6210d316e931bfd06cc3e4992e2881ad963
Dima Sheptytskyi <channektoshka@gmail.com>
2024-05-11 19:45:51 +0300

    feat: user adv

--- volunteer_app_backend/advertisements/views.py
+++ volunteer_app_backend/advertisements/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from advertisements.serializers import AdvertisementCreateSerializer
+from advertisements.models import Advertisement
 
 
 class AdvertisementCreateAPIView(GenericAPIView):
@@ -16,3 +17,13 @@ class AdvertisementCreateAPIView(GenericAPIView):
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MyAdvertisementView(GenericAPIView):
+    serializer_class = AdvertisementCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        advertisement = Advertisement.objects.filter(author_id=request.user.id).first()
+        serializer = self.get_serializer(instance=advertisement)
+        return Response(serializer.data, status=status.HTTP_200_OK)
