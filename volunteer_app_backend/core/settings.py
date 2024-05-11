dd4d80c532ede0f6e331bcb95d9bec830fa86b80
Dima Sheptytskyi <channektoshka@gmail.com>
2024-05-11 17:22:01 +0300

    fix: jwt

--- volunteer_app_backend/core/settings.py
+++ volunteer_app_backend/core/settings.py
@@ -75,6 +75,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
 USE_I18N = True
 
 LANGUAGE_CODE = "uk-Uk"
